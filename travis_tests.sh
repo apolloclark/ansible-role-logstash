bace4f1713740304e54a4ff04612b8d64f79134b
apolloclark <apolloclark@gmail.com>
2019-08-17 12:28:33 -0400

    updates for Amazon Linux 2

--- travis_tests.sh
+++ travis_tests.sh
@@ -23,6 +23,11 @@ export DOCKER_IMAGE_BASE=apolloclark/openjdk:11-centos7.6
 export DOCKER_INST_NAME_SUFFIX=centos_7.6.1810
 pipenv run molecule test --all
 
+# Amazon Linux 2
+export DOCKER_IMAGE_BASE=apolloclark/openjdk:11-amzlinux2
+export DOCKER_INST_NAME_SUFFIX=amzlinux_2
+pipenv run molecule test --all
+
 # Ubuntu Bionic
 export DOCKER_IMAGE_BASE=apolloclark/openjdk:11-ubuntu18.04
 export DOCKER_INST_NAME_SUFFIX=ubuntu_18.04
@@ -31,4 +36,14 @@ pipenv run molecule test --all
 # Ubuntu Xenial
 export DOCKER_IMAGE_BASE=apolloclark/openjdk:11-ubuntu16.04
 export DOCKER_INST_NAME_SUFFIX=ubuntu_16.04
+pipenv run molecule test --all
+
+# Debian 10
+export DOCKER_IMAGE_BASE=apolloclark/openjdk:11-debian10
+export DOCKER_INST_NAME_SUFFIX=debian_10
+pipenv run molecule test --all
+
+# Debian 9
+export DOCKER_IMAGE_BASE=apolloclark/openjdk:11-debian9
+export DOCKER_INST_NAME_SUFFIX=debian_9
 pipenv run molecule test --all
